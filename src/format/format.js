557f9c3f97f1dec7cf0a14c5a870568c46d2f3e3
Luiz Felipe Barbosa <luiz.felipe.b9810@gmail.com>
2018-07-10 22:34:34 -0300

    feat:function format implement

---
+++ src/format/format.js
@@ -0,0 +1,13 @@
+const format = (string) =>{
+  //const removeSpacessides = string.trim()
+  const split1 = string.split('+')//['1-2-3','1']
+  const trim1 = split1.map(x => x.trim())
+  const join1 = trim1.join(' +') //['1-2-3 +1']
+  const split2 = join1.split('-')//['1','2','3 +1']
+  const trim2 = split2.map(x =>x.trim())
+  const join2 = trim2.join(' -')//['1 -2 -3 +1']
+  const trim3 = join2.trim()
+  return trim3
+}
+
+export default format
--- src/format/format.test.js
+++ src/format/format.test.js
@@ -4,3 +4,10 @@ test('Transform the string in array [x, -x, x, x]', () => {
         expect(format('1-2-3+1')).toEqual('1 -2 -3 +1')
 })
 
+test('Transform the string in array [x, -x, x, x]',() => {
+        expect(format('1  -2 -3  +1')).toEqual('1 -2 -3 +1')
+})
+
+test('Transform the string in array [-x, -x, x, x]',() => {
+        expect(format('-1-2-3+1')).toEqual('-1 -2 -3 +1')
+})
