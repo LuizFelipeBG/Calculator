9794566d3dd9101f32140bc6bb7935ba84f0c0e2
Luiz Felipe Barbosa <luiz.felipe.b9810@gmail.com>
2018-07-13 17:25:20 -0300

    function calculator finalized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import calculator from './calculator/calculator.js';
+import format from './format/format.js';
+const initialState = {
+  value:''
+}
 class App extends Component {
+  state = {...initialState}
+  
+  onChangevalue = (event) => {
+     this.setState({value: event.target.value});
+  }
+  submitOn(event){
+    //De alguma forma o evento das fuçoes devem acontecer aqui dentro
+    alert(this.state.value)  
+    event.preventDefault();
+  }
 
   render() {
     return (
+      <form onSubmit={this.submitOn}>
       <div className="App">
-        <input type="text" className='calculatorInput'/>
-        <button type="button" className='calculatorButton'>Calcular</button>
+        <input onChange={this.onChangevalue}/>
+        <button type="submit" value="submit" className='calculatorButton' onClick={this.submitOn}>Calcular</button>
       </div>
+      </form>
     );
   };
 }
-
 export default App;
--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -1,8 +1,8 @@
 
 const calculator = (string) => {
     const split1 = string.split(' ')//['1','+2','-3','+1']
-    const map = split1.map(x => x.Number())
-    const reduce = map.reduce((acc,value) => acc + value)
-    return reduce
+    const map = split1.map(x => parseInt(x))
+    const result = map.reduce((acc,value) => acc + value)
+    return result
 }
 export default calculator
