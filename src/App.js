7427d5aeabf295de69d3cf06479107e19f8e8e2b
Luiz Felipe Barbosa <luiz.felipe.b9810@gmail.com>
2018-07-17 22:56:44 -0300

    finished history calculator

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import result from './result/result';
+import format from './format/format';
 const initialState = {
   value:'',
   theResult:'', 
+  history:[]
 }
+
 
 class App extends Component {
   state = {...initialState}
   
+  
   onChangevalue = (event) => {
-     this.setState({value: event.target.value});
+    this.setState({value: event.target.value});
   }
+
   handleClick = () => {
-    this.setState({theResult: result(this.state.value) || '0', value:''})
+    const history = [...this.state.history]
+    this.setState({theResult: result(this.state.value)})
+    history.push(format(this.state.value))
+    this.setState({history: history})
   }
 
   render() {
     return (
       <div className="App">
-        <input value={this.state.value} onChange={this.onChangevalue}/>
+        <input value={this.state.value} onChange={this.onChangevalue} className='calculatorInput'/>
         <button type="button" className='calculatorButton' onClick={this.handleClick}>Calcular</button>
         <h3>{this.state.theResult}</h3>
+        <p>history: {this.state.history.join('----------------')}</p>
       </div>
       
     );
