a57e4ad8eca1c1e993fdee153547b1001d7442ac
Luiz Felipe Barbosa <luiz.felipe.b9810@gmail.com>
2018-08-08 20:03:11 -0300

    Change color in unexpected value

--- src/App.js
+++ src/App.js
@@ -22,14 +22,16 @@ class App extends Component {
   handleClick = () => {
     if (true === isNaN(result(this.state.value))){
       this.setState({theResult: 'unexpected value'})
+      document.getElementById('demo').style.color = "red";
       return
     }
     if (textInputEnd(this.state.value) === false){
       this.setState({theResult: 'unexpected value'})
+      document.getElementById('demo').style.color = "red"
       return
     }
     
-
+    document.getElementById('demo').style.color = "black"
     const history = [...this.state.history]
     this.setState({theResult: result(this.state.value) || 0, value:''})
     history.push(historyFormat(format(this.state.value)) + ' = ' + (result(this.state.value) ))
@@ -40,17 +42,16 @@ class App extends Component {
     return (
       <div className="App">
         <input value={this.state.value} onChange={this.onChangevalue} className='calculatorInput'/>
-        <button type="button" className='calculatorButton' onClick={this.handleClick}>Calcular</button>
-        <h3>{this.state.theResult}</h3>
+        <button type="submit" className='calculatorButton' onClick={this.handleClick}>Calcular</button>
+        <h3 id='demo'>{this.state.theResult}</h3>
         <p className='historyBox'>History: <br/>
           { this.state.history.map(x => (
             <p>{x}</p>
           ))}
         </p>
-
-      </div>
-
+        </div>
     );
   };
 }
+
 export default App;
